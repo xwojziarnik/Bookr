8f26f7bcc7841621db34968e61a21dd994011998
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-02 11:12:38 +0200

    Added base template & passed variable into template

--- reviews/views.py
+++ reviews/views.py
@@ -1,23 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    """
-    Used name variable to pass an argument into URL to change the text rendered on web page.
-
-    Example:
-
-    Input:
-    https://127.0.0.1:8000
-    Output:
-    Witaj, świecie!
-
-    Input:
-    https://127.0.0.1:8000/?name=Wojtek
-    Output:
-    Witaj, Wojtek!
-    """
-    name = request.GET.get("name") or "świecie"
-    return HttpResponse("Witaj, {}!".format(name))
+    name = "świecie"
+    return render(request, "base.html", {"name": name})
