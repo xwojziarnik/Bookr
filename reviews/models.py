5d86d9edc753289f20710c47dca57a862d315c6a
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-02 13:14:00 +0200

    Added my first model.

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50, help_text="Nazwa wydawnictwa.")
+    website = models.URLField(help_text="Witryna wydawnictwa.")
+    email = models.EmailField(help_text="Adres e-mail wydawnictwa.")
