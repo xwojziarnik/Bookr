0e998d853d3a2bf65658a08b224efbc506775bc9
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-02 10:39:43 +0200

    Added QueryDict into index func

--- reviews/views.py
+++ reviews/views.py
@@ -4,4 +4,20 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    """
+    Used name variable to pass an argument into URL to change the text rendered on web page.
+
+    Example:
+
+    Input:
+    https://127.0.0.1:8000
+    Output:
+    Witaj, świecie!
+
+    Input:
+    https://127.0.0.1:8000/?name=Wojtek
+    Output:
+    Witaj, Wojtek!
+    """
+    name = request.GET.get("name") or "świecie"
+    return HttpResponse("Witaj, {}!".format(name))
