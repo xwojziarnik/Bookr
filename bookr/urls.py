81d91a9d3472ec7b5ab65afec3c30622ac3bfa44
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-04 19:34:32 +0200

    Next view added.

--- bookr/urls.py
+++ bookr/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import reviews.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book-search', reviews.views.book_search),
+    path('', include('reviews.urls')),
+    path('book-search', reviews.views.book_search)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
@@ -19,3 +21,9 @@ def book_search(request):
     """
     search_text = request.GET.get("search", "")
     return render(request, "search-results.html", {"search_text": search_text})
+
+
+def welcome_view(request):
+    message = f"<html><h1>Witaj w witrynie Bookr!</h1>\n" \
+              f"<p>{Book.objects.count()} książek i stale dodajemy nowe!</p></html>"
+    return HttpResponse(message)
