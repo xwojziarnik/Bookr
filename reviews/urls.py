a3fe0e9683d460739e313f267519611476124422
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-05 17:39:07 +0200

    Added detail view.

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import book_list
+from .views import book_list, book_detail
 
 urlpatterns = [
-    path('books/', book_list, name='book_list')
+    path('books/', book_list, name='book_list'),
+    path('books/<int:pk>/', book_detail, name="book_detail")
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book, Review
 from .utils import average_rating
 
@@ -23,3 +23,22 @@ def book_list(request):
             "book_list": book_list
         }
     return render(request, 'book_list.html', context)
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        context = {
+            "book": book,
+            "book_rating": book_rating,
+            "reviews": reviews
+        }
+    else:
+        context = {
+            "book": book,
+            "book_rating": None,
+            "reviews": None
+        }
+    return render(request, "book_detail.html", context)
