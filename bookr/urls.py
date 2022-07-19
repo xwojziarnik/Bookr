dd01fe3d3be47c6ac45a4c16f6ce04b04103718e
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-19 19:37:27 +0200

    Created accounts features.

--- bookr/urls.py
+++ bookr/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from bookr import settings
 
 urlpatterns = [
+    path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ]
