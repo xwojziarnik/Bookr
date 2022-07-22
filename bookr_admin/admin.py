7196e46efae6454b0821c3380bde62aa41db81f2
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-22 07:39:33 +0200

    Trying to add created view into admin site, got 404.

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
 
 # Register your models here.
 
@@ -8,3 +10,13 @@ class BookrAdmin(admin.AdminSite):
     site_title = "Moja aplikacja internetowa Django"
     index_title = "Panel administracyjny"
     logout_template = "admin/logout.html"
+
+    def profile_view(self, request):
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(request, "admin/admin_profile.html", context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        url_patterns = [path("admin_profile", self.admin_view(self.profile_view))]
+        return urls + url_patterns
