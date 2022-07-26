9bd774aa5c70cf3f4ea978c18c8fa05d0ff4101f
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-26 07:44:28 +0200

    CRUD - now delete view - done.

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BookCreateView, FormSuccessView
+from .views import BookCreateView, FormSuccessView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('book-record-create/', BookCreateView.as_view(), name="book_create"),
-    path('book-record-create/entry-success/', FormSuccessView.as_view(), name="form_success")
+    path('entry-success/', FormSuccessView.as_view(), name="form_success"),
+    path('book-record-update/<int:pk>/', BookUpdateView.as_view(), name="book_update"),
+    path('book-record-delete/<int:pk>/', BookDeleteView.as_view(), name="book_delete"),
 ]
--- book_management/views.py
+++ book_management/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
@@ -10,9 +10,22 @@ class BookCreateView(CreateView):
     model = Book
     fields = ["name", "author"]
     template_name = "book_form.html"
-    success_url = "entry-success/"
+    success_url = "/book-management/entry-success/"
 
 
 class FormSuccessView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse("Rekord modelu Book został zapisany.")
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    fields = ["name", "author"]
+    template_name = "book_form.html"
+    success_url = "/book-management/entry-success/"
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = "book_delete_form.html"
+    success_url = "/book-management/delete-success/"
