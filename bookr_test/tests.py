85f02dd2cd94fc094eaa13fc12f762e41154a7ca
xwojziarnik <woj.ziarnik@gmail.com>
2022-07-29 07:45:50 +0200

    First tests passed.

--- bookr_test/tests.py
+++ bookr_test/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Publisher
 
 # Create your tests here.
+
+
+class TestPublisherModel(TestCase):
+    def setUp(self):
+        self.p = Publisher(name="Packt",
+                           website="www.packt.com",
+                           email="contact@packt.com")
+
+    def test_create_publisher(self):
+        self.assertIsInstance(self.p, Publisher)
+
+    def test_str_representation(self):
+        self.assertEquals(str(self.p), "Packt")
